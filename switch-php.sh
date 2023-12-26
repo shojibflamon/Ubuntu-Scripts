d56292f76b8cfd0fc180706558c3c3797d754fd0
Shojib Flamon <shojib.seu@gmail.com>
2023-12-26 19:28:29 +0600

    Switch PHP nginx & apache with arg

--- switch-php.sh
+++ switch-php.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
 
+webServer=$1
+version=$2
 
-version=$1
+if [ "$webServer" == "nginx" ]; then
+   if [ "$version" == "8.2" ]; then
+      echo "* Switching CLI PHP to 8.2 ..."
+      sudo update-alternatives --set php /usr/bin/php8.2 > /dev/null
+      echo "* Switch to PHP 8.2 complete."
 
-if [ "$version" == "7.4" ]; then
-  echo "* Disabling Apache PHP 8.2 module ..."
-  sudo a2dismod php8.2 > /dev/null
-elif [ "$version" == "8.2" ]; then
-    echo "* Disabling Apache PHP 7.4 module ..."
+      echo "* Restart $webServer"
+      sudo nginx -s reload
+    elif [ "$version" == "7.4" ]; then
+      echo "* Switching CLI PHP to 7.4 ..."
+        sudo update-alternatives --set php /usr/bin/php7.4 > /dev/null
+        echo "* Switch to PHP 7.4 complete."
+
+        echo "* Restart $webServer"
+        sudo nginx -s reload
+    else
+      echo "Nothing to switch to..."
+    fi
+elif [ "$webServer" == "apache" ]; then
+  if [ "$version" == "8.2" ]; then
+      echo "* Disabling Apache PHP 7.4 module ..."
       sudo a2dismod php7.4 > /dev/null
 
       echo "* Enabling Apache PHP 8.2 module ..."
@@ -15,13 +31,29 @@ elif [ "$version" == "8.2" ]; then
 
       echo "* Switching CLI PHP to 8.2 ..."
       sudo update-alternatives --set php /usr/bin/php8.2 > /dev/null
-
       echo "* Switch to PHP 8.2 complete."
 
-      echo "* Restart apache"
+      echo "* Restart $webServer"
       sudo systemctl restart apache2.service
+  elif [ "$version" == "7.4" ]; then
+    echo "* Disabling Apache PHP 8.2 module ..."
+    sudo a2dismod php8.2 > /dev/null
+
+    echo "* Enabling Apache PHP 7.4 module ..."
+    sudo a2enmod php7.4 > /dev/null
+
+    echo "* Switching CLI PHP to 7.4 ..."
+    sudo update-alternatives --set php /usr/bin/php7.4 > /dev/null
+    echo "* Switch to PHP 7.4 complete."
+
+    echo "* Restart $webServer"
+    sudo systemctl restart apache2.service
+  else
+      echo "Nothing to switch to..."
+  fi
 else
-  echo "Nothing to switch to..."
+    echo "No Webserver selected..."
 fi
 
-
+echo "* PHP Version Now..."
+sudo php -v
